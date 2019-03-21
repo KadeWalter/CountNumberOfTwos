cf140678b58fa44a8e9bda06cf191d7f6fc6be3c
Kade Walter <kadwalter1@gmail.com>
2019-03-21 12:19:56 -0500

    implementation
    implemented the code. Counts all 2's found from 0 to the num given. 23 = 7, 323 = 167

---
+++ c#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 323;
+            int count = CountAllTwos(number);
+
+            Console.WriteLine("\nCount of 2s: " + count + "\n\n");
+        }
+
+        private static int CountAllTwos(int num)
+        {
+            int count = 0;
+            for(int i = 0; i <= num; i++)
+            {
+                count += TwoInNum(i);
+            }
+            return count;
+        }
+
+        private static int TwoInNum(int num)
+        {
+            int count = 0;
+            while(num > 0)
+            {
+                if(num % 10 == 2)
+                    count++;
+                num = num / 10;
+            }
+            return count;
+        }
+    }
+}

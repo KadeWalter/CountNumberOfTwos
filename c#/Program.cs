04965b5b16016e7b6959d28375288b8cd2a30b10
Kade Walter <kadwalter1@gmail.com>
2019-03-24 14:17:39 -0500

    implementation

--- c#/Program.cs
+++ c#/Program.cs
@@ -33,5 +33,29 @@ namespace c_
             }
             return count;
         }
+
+        private static int NumOfTwosFromNum(int num)
+        {
+            int count = 0;
+            // 323 = 167
+            // 23 = 7
+
+            // 300 means 100 2's just from 200
+            // 10 2's from 0 to 100 in the 1's place
+            // 10 2's from 0 to 100 in the 10's place
+            
+
+            // FOR 23: TO GET 7
+            // 10's place count = (higher(0 in this case) * 10) + (lower + 1)
+            // -> (0 * 10) + (3 + 1) = 4
+            // 1's place count = (highest * 10) + (higher + 1 if >= 2 in row, higher if < 2 in row) 
+            // -> (0 * 10) + (2 + 1) = 3
+
+            string numAsString = num.ToString();
+            int[] arr = new int[numAsString.Length];
+            for(int i = 0; i < numAsString.Length; i++)
+                arr[i] = Convert.ToInt32(numAsString.Substring(i,1));
+            return count;
+        }
     }
 }
